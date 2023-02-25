cd816213d024d1bdde9a6c98668d8d7bca913a8f
Sandip Sadhukhan <sandippakhanna@gmail.com>
2023-02-25 22:18:21 +0530

    fonts loaded

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/600.css";
+
 import theme from "@/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
